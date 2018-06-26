9c9fa062d025a7633e931f9085f2868325bc3c91
Cvetoslav Ludmiloff <ludmiloff@gmail.com>
2018-06-26 22:40:54 +0300

    fix: pass 'route' when hash = #/route

--- src/router.ts
+++ src/router.ts
@@ -10,17 +10,23 @@ export default function route(url: string) {
   if (!url) url = '#';
   if (url.startsWith('#')) {
     const [name, ...rest] = url.split('/');
-    if (name === "#" && app.onRoute(rest[0], rest.shift()) || app.onRoute(name, ...rest))
+    if (name === "#") {
+      if (app.onRoute(rest[0], rest.shift())) {
+        app.run(name, ...rest)
+      }
+      return;
+    }
+
+    if (app.onRoute(name, ...rest)) {
       app.run(name, ...rest);
-    //app.run(ROUTER_EVENT, name, ...rest);
+    }
+
   } else if (url.startsWith('/')) {
     const [_, name, ...rest] = url.split('/');
     if (app.onRoute('/' + name, ...rest))
       app.run('/' + name, ...rest);
-    //app.run(ROUTER_EVENT, '/' + name, ...rest);
   } else {
     if (app.onRoute(url))
-      app.run(url);
-    //app.run(ROUTER_EVENT, url);
+      app.run(url);    
   }
 }
