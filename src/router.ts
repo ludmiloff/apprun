cf73288333144abadc51b4cd63927bf40d54c0ea
Cvetoslav Ludmiloff <ludmiloff@gmail.com>
2018-06-30 20:35:21 +0300

    keep to original idea as close as possible

--- src/router.ts
+++ src/router.ts
@@ -3,19 +3,21 @@ import app from './app';
 const ROUTER_EVENT = '//';
 
 //app.on('//', _ => { });
-app.on('#', _ => { });
+app.on('/', _ => { });
+app.on('#', _ => app.run('/'));
+
 app.on('route', url => route(url));
 
 export default function route(url: string) {
-  if (!url) url = '#';
+  if (!url || url === '/') url = '#';
   if (url.startsWith('#')) {
     const [name, ...rest] = url.split('/');
-    if (name === "#") {
-      if (app.onRoute(rest[0], rest.shift())) {
-        app.run(name, ...rest)
-      }
-      return;
-    }
+    // if (name === "#") {
+    //   if (app.onRoute(rest[0], rest.slice(1))) {
+    //     app.run(name, ...rest)
+    //   }
+    //   return;
+    // }
 
     if (app.onRoute(name, ...rest)) {
       app.run(name, ...rest);
